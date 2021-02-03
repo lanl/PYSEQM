615856ce9105c09817fe5a9e1a414715d2dc1fe1
GuoqingZhou-LANL <kuoching.chou@gmail.com>
2021-02-03 02:03:09 -0700

    fix bugs

--- setup.py
+++ setup.py
@@ -57,5 +57,8 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['numpy', 'torch>=1.2'],
+    data_files=[('seqm/params', ['parameters_AM1_MOPAC.csv', 
+                                 'parameters_MNDO_MOPAC.csv', 
+                                 'parameters_PM3_MOPAC.csv'])],
 
 )
