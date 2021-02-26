fix gradient bugs

seqm/basics.py
@@ -350,14 +350,22 @@ class Force(torch.nn.Module):
         L = Hf.sum()
         if const.do_timing:
             t0 = time.time()
-        gv = [coordinates]
 
-        gradients  = grad(L, gv,create_graph=self.create_graph)
+        #gv = [coordinates]
+        #gradients  = grad(L, gv,create_graph=self.create_graph)
+        L.backward(create_graph=self.create_graph)
         if const.do_timing:
             if torch.cuda.is_available():
                 torch.cuda.synchronize()
             t1 = time.time()
             const.timing["Force"].append(t1-t0)
-        force = -gradients[0]
+        #force = -gradients[0]
+        if self.create_graph:
+            force = -coordinates.grad.clone()
+            with torch.no_grad():
+                coordinates.grad.zero_()
+        else:
+            force = -coordinates.grad.detach()
+            coordinates.grad.zero_()
 
         return force, P, Etot, Hf, Eelec, Enuc, Eiso, EnucAB, e, charge, notconverged