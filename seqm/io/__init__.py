9f8662aaefd9fff4484584f359e857efb3a5510e
Vishikh Athavale <vishikh1@gmail.com>
2026-08-10 18:04:10 -0600

    Add MNDO-type CIS transition density cube export

--- seqm/io/__init__.py
+++ seqm/io/__init__.py
@@ -1,3 +1,10 @@
+from .cube import cis_transition_density_ao, evaluate_mndo_sto_basis, write_cis_transition_density_cubes
 from .xyz import read_xyz, save_xyz
 
-__all__ = ["read_xyz", "save_xyz"]
+__all__ = [
+    "read_xyz",
+    "save_xyz",
+    "cis_transition_density_ao",
+    "evaluate_mndo_sto_basis",
+    "write_cis_transition_density_cubes",
+]
